969384329c35ad9b6df23dc04c7ab7f099470abc
OutisMed <medardkik01@gmail.com>
2024-05-28 12:43:06 +0200

    ajoutez des vérifications pour MarsUiState.Loading

--- marsphotos/app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
+++ marsphotos/app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
@@ -17,6 +17,8 @@ package com.example.marsphotos.ui.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,12 +32,29 @@ import com.example.marsphotos.ui.theme.MarsPhotosTheme
 
 @Composable
 fun HomeScreen(
-    marsUiState: String,
-    modifier: Modifier = Modifier,
-    contentPadding: PaddingValues = PaddingValues(0.dp),
+    marsUiState: MarsUiState,
+    modifier: Modifier = Modifier
 ) {
-    ResultScreen(marsUiState, modifier.padding(top = contentPadding.calculateTopPadding()))
+    when (marsUiState) {
+        is MarsUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
+        is MarsUiState.Success -> ResultScreen(
+            marsUiState.photos, modifier = modifier.fillMaxWidth()
+        )
+
+        is MarsUiState.Error -> ErrorScreen( modifier = modifier.fillMaxSize())
+    }
 }
+
+@Composable
+fun ErrorScreen(modifier: Modifier) {
+
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier) {
+
+}
+
 
 /**
  * ResultScreen displaying number of photos retrieved.
