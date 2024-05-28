13d4cb90bdf0df461d03903ae403332eefcdc711
OutisMed <medardkik01@gmail.com>
2024-05-28 12:45:44 +0200

    ajoutez la fonction modulable LoadingScreen

--- marsphotos/app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
+++ marsphotos/app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
@@ -16,10 +16,15 @@
 package com.example.marsphotos.ui.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -51,8 +56,12 @@ fun ErrorScreen(modifier: Modifier) {
 }
 
 @Composable
-fun LoadingScreen(modifier: Modifier) {
-
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
 }
 
 
