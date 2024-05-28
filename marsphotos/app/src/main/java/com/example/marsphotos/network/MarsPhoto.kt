824a92c870bb64bbb9938070d637a0af98143e9a
OutisMed <medardkik01@gmail.com>
2024-05-28 13:04:20 +0200

    Remplacez la ligne de la clé img_src

--- marsphotos/app/src/main/java/com/example/marsphotos/network/MarsPhoto.kt
+++ marsphotos/app/src/main/java/com/example/marsphotos/network/MarsPhoto.kt
@@ -1,5 +1,9 @@
 package com.example.marsphotos.network
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@SerialName(value = "img_src")
 data class MarsPhoto(
     val id: String,  val img_src: String
 )
